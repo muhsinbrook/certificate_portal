51ec4936c195d7f577c699cbd1968be6b031edc1
muhsinbrook <muhsinmedayilms@gmail.com>
2026-08-31 18:09:04 +0530

    Register certificate model safely in Django admin

--- certificateapp/admin.py
+++ certificateapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CertificateApp
+
+
+@admin.register(CertificateApp)
+class CertificateAppAdmin(admin.ModelAdmin):
+    list_display = (
+        "certificate_id",
+        "recipient_name",
+        "program_name",
+        "principal_name",
+        "issue_date",
+    )
+    search_fields = (
+        "certificate_id",
+        "recipient_name",
+        "program_name",
+        "principal_name",
+    )
+    list_filter = ("program_name", "issue_date")
+    readonly_fields = ("certificate_id", "college_name", "created_at")
