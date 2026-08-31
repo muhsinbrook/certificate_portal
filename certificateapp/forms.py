195c80ee44bd72ebbba94de5b9c4538d8ba7b2a8
muhsinbrook <muhsinmedayilms@gmail.com>
2026-08-31 17:45:50 +0530

    Add course dropdown and make college name permanent

--- certificateapp/forms.py
+++ certificateapp/forms.py
@@ -3,6 +3,32 @@ from .models import CertificateApp
 
 
 class CertificateForm(forms.ModelForm):
+    PROGRAM_CHOICES = [
+        ("Computer Engineering", "Computer Engineering"),
+        ("Electrical & Electronics Engineering", "Electrical & Electronics Engineering"),
+        ("Electrical Engineering", "Electrical Engineering"),
+        ("Mechanical Engineering", "Mechanical Engineering"),
+        ("Computer Hardware Engineering", "Computer Hardware Engineering"),
+        ("Electronics Communication Engineering", "Electronics Communication Engineering"),
+    ]
+
+    program_name = forms.ChoiceField(
+        choices=PROGRAM_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control"}),
+        label="Program / Course Name",
+    )
+
+    college_name = forms.CharField(
+        initial="Model Polytechnic College Karunagappally",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "readonly": "readonly",
+            }
+        ),
+        label="College Name",
+    )
+
     class Meta:
         model = CertificateApp
         fields = [
@@ -21,32 +47,16 @@ class CertificateForm(forms.ModelForm):
                     "placeholder": "Jane A. Doe",
                 }
             ),
-
-            "program_name": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Bachelor of Science in Computer Science",
-                }
-            ),
-
-            "college_name": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                }
-            ),
-
             "dean_name": forms.TextInput(
                 attrs={
                     "class": "form-control",
                 }
             ),
-
             "registrar_name": forms.TextInput(
                 attrs={
                     "class": "form-control",
                 }
             ),
-
             "issue_date": forms.DateInput(
                 attrs={
                     "class": "form-control",
@@ -54,3 +64,6 @@ class CertificateForm(forms.ModelForm):
                 }
             ),
         }
+
+    def clean_college_name(self):
+        return "Model Polytechnic College Karunagappally"
